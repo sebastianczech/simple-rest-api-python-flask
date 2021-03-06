5fc71480891431b74535be6a4d0c46c8d2a3c1f0
Sebastian Czech <sebaczech@gmail.com>
2021-03-06 09:37:00 +0100

    Fix flask test with context

--- test_people.py
+++ test_people.py
@@ -1,16 +1,36 @@
 import people
+import connexion
+import pytest
 
 
-def test_people_add_and_read_all():
-    person = {
-        "fname": "Test Name",
-        "lname": "Test Lastname",
-        "timestamp": people.get_timestamp(),
-    }
-    list = people.create(person)
-    assert 4 == len(list)
+app = connexion.App(__name__, specification_dir='specification')
+app.add_api('swagger.yml')
 
 
-def test_people_read_all():
-    list_of_people = people.read_all()
-    assert 3 == len(list_of_people)
+@pytest.fixture
+def client():
+    with app.app.test_client() as client:
+        yield client
+
+
+def test_people_add_and_read_all(client):
+    with app.app.app_context():
+        person = {
+            "fname": "Test Name",
+            "lname": "Test Lastname",
+            "timestamp": people.get_timestamp(),
+        }
+        people.create(person)
+        list_of_people = people.read_all()
+        assert 4 == len(list_of_people)
+
+        lname = "Test Lastname"
+        people.delete(lname)
+        list_of_people = people.read_all()
+        assert 3 == len(list_of_people)
+
+
+def test_people_read_all(client):
+    with app.app.app_context():
+        list_of_people = people.read_all()
+        assert 3 == len(list_of_people)
